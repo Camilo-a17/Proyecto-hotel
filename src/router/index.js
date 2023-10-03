037014a538e8d0db105bf8e93adec189b86c6d9c
moises <contact@correo.com>
2023-10-03 14:27:31 -0500

    add Home

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,14 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/prin',
+      name: 'principal',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HomePrinView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
