c2a5218dd9ff5edaf1a78404b2b8e7a8fb5443b9
moises <contact@correo.com>
2023-10-05 13:11:46 -0500

    setting footer

--- src/components/global/NavComp.vue
+++ src/components/global/NavComp.vue
@@ -15,7 +15,7 @@
 <template>
   <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <router-link to="/" class="flex items-center">
+      <router-link to="/prin" class="flex items-center">
          <font-awesome-icon icon="fa-solid fa-square-h" shake size="2xl" style="color: #025cf7;" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-blue-800">OTELES</span>
       </router-link>
