1d8d76808149d85155d953180042b8aa63040530
camilo-a17 <camilko99a@gmail.com>
2023-10-16 17:30:14 -0500

    Componentes de inicio

---
+++ src/components/ReporteHoteles.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="mt-9 p-10">
+    <div class="flex flex-col justify-center">
+      <div class="flex">
+        <!-- Gráfico de habitaciones por tipo -->
+        <div class="w-full max-w-lg bg-white shadow-lg rounded-sm border border-gray-200 mr-8">
+          <header class="px-5 py-4 border-b border-gray-100">
+            <h2 class="font-semibold text-gray-800">Habitaciones por Tipo</h2>
+          </header>
+          <div class="p-3">
+            <bar-chart
+              :data="habitacionesPorTipoData"
+              :options="habitacionesPorTipoOptions"
+            ></bar-chart>
+          </div>
+        </div>
+
+        <!-- Gráfico de total de habitaciones por hotel -->
+        <div class="w-full max-w-lg bg-white shadow-lg rounded-sm border border-gray-200">
+          <header class="px-5 py-4 border-b border-gray-100">
+            <h2 class="font-semibold text-gray-800">Total de Habitaciones por Hotel</h2>
+          </header>
+          <div class="p-3">
+            <bar-chart
+              :data="habitacionesPorHotelData"
+              :options="habitacionesPorHotelOptions"
+            ></bar-chart>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  components: {
+    BarChart: Bar
+  },
+  data() {
+    return {
+      habitacionesPorTipoData: {
+        labels: ['Estándar', 'Junior', 'Suite'],
+        datasets: [
+          {
+            label: 'Hotel A',
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+            data: [30, 20, 10]
+          },
+          {
+            label: 'Hotel B',
+            backgroundColor: ['#4BC0C0', '#FF5733', '#7CB342'],
+            data: [20, 10, 10]
+          }
+        ]
+      },
+      habitacionesPorTipoOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            type: 'category'
+          },
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+      habitacionesPorHotelData: {
+        labels: ['Hotel A', 'Hotel B'],
+        datasets: [
+          {
+            label: 'Total de Habitaciones',
+            backgroundColor: ['#36A2EB', '#FFCE56'],
+            data: [60, 40]
+          }
+        ]
+      },
+      habitacionesPorHotelOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            type: 'category'
+          },
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    }
+  },
+  mounted() {
+    this.renderChart(this.habitacionesPorTipoData, this.habitacionesPorTipoOptions)
+    this.renderChart(this.habitacionesPorHotelData, this.habitacionesPorHotelOptions)
+  }
+}
+</script>
+  
--- src/views/InicioDasboardView.vue
+++ src/views/InicioDasboardView.vue
@@ -1,60 +1,10 @@
-<template>
-    <div class="flex flex-wrap bg-slate-100 justify-center">
-            <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words border-t-4 bg-white rounded mb-3 xl:mb-0 shadow-lg">
-                    <div class="flex-auto p-4 pb-12">
-                        <div class="flex flex-wrap">
-                            <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                                <h5 class="text-blueGray-400 uppercase font-bold text-xs"> Hoteles</h5>
-                                <span class="font-semibold text-xl text-blueGray-700">20</span>
-                            </div>
-                            <div class="relative w-auto pl-4 flex-initial">
-                                <a href="/list"
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 32C0 14.3 14.3 0 32 0H480c17.7 0 32 14.3 32 32s-14.3 32-32 32V448c17.7 0 32 14.3 32 32s-14.3 32-32 32H304V464c0-26.5-21.5-48-48-48s-48 21.5-48 48v48H32c-17.7 0-32-14.3-32-32s14.3-32 32-32V64C14.3 64 0 49.7 0 32zm96 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V112c0-8.8-7.2-16-16-16H112c-8.8 0-16 7.2-16 16zM240 96c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V112c0-8.8-7.2-16-16-16H240zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V112c0-8.8-7.2-16-16-16H368c-8.8 0-16 7.2-16 16zM112 192c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V208c0-8.8-7.2-16-16-16H112zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V208c0-8.8-7.2-16-16-16H240c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V208c0-8.8-7.2-16-16-16H368zM328 384c13.3 0 24.3-10.9 21-23.8c-10.6-41.5-48.2-72.2-93-72.2s-82.5 30.7-93 72.2c-3.3 12.8 7.8 23.8 21 23.8H328z"/></svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words border-t-4 bg-white rounded mb-3 xl:mb-0 shadow-lg">
-                    <div class="flex-auto p-4 pb-12">
-                        <div class="flex flex-wrap">
-                            <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                                <h5 class="text-blueGray-400 uppercase font-bold text-xs"> Habitaciones</h5>
-                                <span class="font-semibold text-xl text-blueGray-700">50</span>
-                            </div>
-                            <div class="relative w-auto pl-4 flex-initial">
-                                <a href="/habitaciones"
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><path d="M32 32c17.7 0 32 14.3 32 32V320H288V160c0-17.7 14.3-32 32-32H544c53 0 96 43 96 96V448c0 17.7-14.3 32-32 32s-32-14.3-32-32V416H352 320 64v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V64C0 46.3 14.3 32 32 32zm144 96a80 80 0 1 1 0 160 80 80 0 1 1 0-160z"/></svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-4 w-full lg:w-6/12 xl:w-3/12 px-5 mb-4">
-                <div class="relative flex flex-col min-w-0 break-words border-t-4 bg-white rounded mb-3 xl:mb-0 shadow-lg">
-                    <div class="flex-auto p-4 pb-12">
-                        <div class="flex flex-wrap">
-                            <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                                <h5 class="text-blueGray-400 uppercase font-bold text-xs"> Usuarios</h5>
-                                <span class="font-semibold text-xl text-blueGray-700">10</span>
-                            </div>
-                            <div class="relative w-auto pl-4 flex-initial">
-                                <a href="#"
-                                    class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full  bg-blue-500">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z"/></svg>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<script setup>
 
-            
+import inicioDasboard from '../components/InicioDasboardComp.vue'
+import ReportesHotel from '../components/ReporteHoteles.vue'
+</script>
+
+<template> 
+<inicioDasboard /> 
+<ReportesHotel /> 
 </template>
